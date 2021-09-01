b2e1941c9c826c62a2a757a2cccadf722a8e83c7
Aary Angre <87111984+YoBoii07@users.noreply.github.com>
2021-09-01 22:57:52 +0530

    Create calculator.rb

---
+++ calculator.rb
@@ -0,0 +1,32 @@
+counter = 1
+while counter = 1
+  #first step is to get the inputs
+  puts "please enter the 1st number"
+  num1 = gets.chomp().to_f
+  puts "please enter the 2nd number"
+  num2 = gets.chomp().to_f
+#ruby adds a \n by default to every input .chomp() method gets
+#rid of that and .to_f method converts input to floating point num
+
+  puts "please enter the desired operation sign"
+  op = gets.chomp()
+
+#now for the functionality
+#case statement must help to do the operation
+#a loop will help to get the code together
+
+  case op
+    when "+"
+    puts (num1 + num2)
+    
+    when "-"
+    puts (num1 - num2)
+    
+    when "*"
+    puts (num1 / num2)
+    
+    when "÷"
+    puts (num1 / num2)
+    
+    end
+end
